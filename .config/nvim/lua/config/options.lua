c6b605150559b3f54eaf1c2853c40a270c618dae
AkashJana18 <akashjana663@gmail.com>
2026-09-06 13:59:15 +0530

    chore(nvim): disable scroll animation and update dash header
    - config/options.lua: disable snacks scroll animation globally
    - plugins/dash.lua: update header to AKASH CODE style
    - plugins/snacks.lua: reformat keys, fix typo scorll -> remove
    - mini.animate already removed from lazyvim.json and lazy-lock.json clean
      (no directory at ~/.local/share/nvim/lazy/mini.animate)

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,12 @@
 -- Add any additional options here
 
 vim.opt.guifont = "FiraCode Nerd Font:h16"
+
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+}
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -12,13 +12,55 @@ return {
       },
     },
     keys = {
-      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Live Grep" },
-      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-      { "<leader>fh", function() Snacks.picker.help() end, desc = "Help Tags" },
-      { "<leader>fp", function() Snacks.picker.files({ cwd = require("lazy.core.config").options.root }) end, desc = "Find Plugin File" },
-      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
-      { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>fg",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "Live Grep",
+      },
+      {
+        "<leader>fb",
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>fh",
+        function()
+          Snacks.picker.help()
+        end,
+        desc = "Help Tags",
+      },
+      {
+        "<leader>fp",
+        function()
+          Snacks.picker.files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>fr",
+        function()
+          Snacks.picker.recent()
+        end,
+        desc = "Recent Files",
+      },
+      {
+        "<leader>fc",
+        function()
+          Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+        end,
+        desc = "Find Config File",
+      },
     },
   },
 }
