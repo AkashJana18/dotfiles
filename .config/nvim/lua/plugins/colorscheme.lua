8289a6a1b9b0a6e92eab5a5711e9d2a0b9b8eb93
AkashJana18 <akashjana663@gmail.com>
2026-09-17 03:25:21 +0530

    feat(nvim): switch to zenwritten via zenbones.nvim
    - LazyVim colorscheme gruvbox → zenwritten
    - add zenbones-theme/zenbones.nvim + lush with darken_comments 45, darkness warm, lightness dim
    - lazy-lock gruvbox 154eb5f → zenbones 8304d8d

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,9 +2,10 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "zenwritten",
     },
   },
+
   {
     "ellisonleao/gruvbox.nvim",
     name = "gruvbox",
@@ -14,5 +15,23 @@ return {
       terminal_colors = true,
       contrast = "hard", -- hard/soft or "" for default
     },
+  },
+
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+      vim.g.zenbones_darken_comments = 45
+      vim.o.background = "dark" -- required for darkness to apply
+      vim.g.zenwritten_darkness = "warm" -- stark or "warm"
+      vim.g.zenwritten_lightness = "dim" -- bright or dim only if background=light
+      vim.cmd.colorscheme("zenwritten")
+    end,
   },
 }
